b7e99e1858f15ee609e10ff04dd47e89f07d1d0d
unknown <raohamza936@gmail.com>
2025-02-07 15:57:06 +0500

    header modify

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
       <div className="bg-gradient-to-r from-purple-500 to-indigo-500 text-white rounded-t-lg py-5">
-        <h1 className='text-3xl font-bold text-center '>User Infofrmation</h1>
+        <h1 className='text-3xl font-bold text-center '>User Information</h1>
       </div>
     )
   }
